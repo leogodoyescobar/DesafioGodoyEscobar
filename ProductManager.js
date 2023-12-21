f7d80b0250d4adc565a997b532007caf2204c801
Leonel Godoy Escobar <leonel.godoy@gmail.com>
2023-12-21 18:50:35 -0300

    cambiando funcion addProduct con parámetros nombrados

--- ProductManager.js
+++ ProductManager.js
@@ -4,7 +4,7 @@ class ProductManager{
         this.id = 1;
     }
         
-    AddProduct ( title, description, price, thumbnail, code, stock ){
+    AddProduct ( {title, description, price, thumbnail, code, stock} ){
         if (!title || !description || !price || !thumbnail || !code || !stock) {
             console.log(`Todos los campos son obligatorios, el producto ${title} no se agrego.`);
         } else if (!this.products.some((p) => p.code === code )){
@@ -36,15 +36,44 @@ const product = new ProductManager()
 
 console.log( product.getProducts());
 
-product.AddProduct("Royal canin perro", "alimento balanceado", 23000, "url-img", 2000, 10);
-product.AddProduct("pedigree perro", "alimento balanceado", 21500, "url-img", 2001, 15);
-product.AddProduct("Excelent gato", "alimento balanceado", 19000, "url-img", 3000, 14);
+product.AddProduct({
+    title: "Royal canin perro",
+    description: "alimento balanceado",
+    price: 23000,
+    thumbnail: "url-img",
+    code: 2000,
+    stock: 10});
+product.AddProduct({
+    title: "pedigree perro",
+    description: "alimento balanceado",
+    price: 21500,
+    thumbnail: "url-img",
+    code: 2001,
+    stock: 15});
+product.AddProduct({
+    title: "Excelent gato",
+    description: "alimento balanceado",
+    price: 19000,
+    thumbnail: "url-img",
+    code: 3000,
+    stock: 14});
 
 // Producto con datos incompletos
-product.AddProduct("Cat-Chow gato", "alimento balanceado", 19800, "url-img", 3005);
+product.AddProduct({
+    title: "Cat-Chow gato",
+    description: "alimento balanceado",
+    price: 19800,
+    thumbnail: "url-img",
+    code: 3005});
 
 // Prueba de repeticion de parametro code
-product.AddProduct("Royal canin perro", "alimento balanceado", 18700, "url-img", 2000, 14);
+product.AddProduct({
+    title: "Royal canin perro",
+    description: "alimento balanceado",
+    price: 18700,
+    thumbnail: "url-img",
+    code: 2000,
+    stock: 14});
 
 console.log( product.getProducts());
 
