ec4f3b0bb481d7684ebaf86d273b3bf719c177ed
Leonel Godoy Escobar <leonel.godoy@gmail.com>
2024-01-11 20:53:00 -0300

    configurando express servidores

--- ProductManager.js
+++ ProductManager.js
@@ -3,6 +3,7 @@ class ProductManager{
     constructor(){
         this.products = [];
         this.id = 1;
+        this.path = './productos.json'
     }
 
     AddProduct ( {title, description, price, thumbnail, code, stock} ){
@@ -34,12 +35,33 @@ class ProductManager{
     writeToFile(test) {
         fs.promises.writeFile( test, JSON.stringify(this.products, null, 2), { encoding: 'utf-8' } )
         .then( resp => {
-            console.log('Archivo txt creado')
+            console.log('Archivo productos.json creado correctamente')
         })
         .catch( err => {
             console.log('Error al crear archivo', err)
         })
     }
+
+    async readFile() {
+        try {
+            let res = await fs.promises.readFile(this.path, 'utf-8');
+            console.log('Lectura de archivo productos.json a continuacion:', res);
+            return res
+        } catch (err) {
+            console.log('Error:', err);
+        }
+    }
+
+    async readFileById(id) {
+        try {
+            let res = await fs.promises.readFile(this.path, 'utf-8');
+            let product = this.products.find(pr => pr.id === id);
+            return product
+        } catch (err) {
+            console.log('Error:', err);
+        }
+    }
+
     
     updateProduct(id, { title, description, price, thumbnail, code, stock }) {
         const productIndex = this.products.findIndex(product => product.id === id);
@@ -60,79 +82,81 @@ class ProductManager{
     }
 }
 const path = "./productos.json";
-const product = new ProductManager()
+// const product = new ProductManager()
 
-console.log( product.getProducts());
+// console.log( product.getProducts());
 
-product.AddProduct({
-    title: "Royal canin perro",
-    description: "alimento balanceado",
-    price: 23000,
-    thumbnail: "url-img",
-    code: 2000,
-    stock: 10});
-product.AddProduct({
-    title: "pedigree perro",
-    description: "alimento balanceado",
-    price: 21500,
-    thumbnail: "url-img",
-    code: 2001,
-    stock: 15});
-product.AddProduct({
-    title: "Excelent gato",
-    description: "alimento balanceado",
-    price: 19000,
-    thumbnail: "url-img",
-    code: 3000,
-    stock: 14});
+// product.AddProduct({
+//     title: "Royal canin perro",
+//     description: "alimento balanceado",
+//     price: 23000,
+//     thumbnail: "url-img",
+//     code: 2000,
+//     stock: 10});
+// product.AddProduct({
+//     title: "pedigree perro",
+//     description: "alimento balanceado",
+//     price: 21500,
+//     thumbnail: "url-img",
+//     code: 2001,
+//     stock: 15});
+// product.AddProduct({
+//     title: "Excelent gato",
+//     description: "alimento balanceado",
+//     price: 19000,
+//     thumbnail: "url-img",
+//     code: 3000,
+//     stock: 14});
 
 // Producto con datos incompletos
-product.AddProduct({
-    title: "Cat-Chow gato",
-    description: "alimento balanceado",
-    price: 19800,
-    thumbnail: "url-img",
-    code: 3005});
+// product.AddProduct({
+//     title: "Cat-Chow gato",
+//     description: "alimento balanceado",
+//     price: 19800,
+//     thumbnail: "url-img",
+//     code: 3005});
 
 // Prueba de repeticion de parametro code
-product.AddProduct({
-    title: "Royal canin perro",
-    description: "alimento balanceado",
-    price: 18700,
-    thumbnail: "url-img",
-    code: 2000,
-    stock: 14});
+// product.AddProduct({
+//     title: "Royal canin perro",
+//     description: "alimento balanceado",
+//     price: 18700,
+//     thumbnail: "url-img",
+//     code: 2000,
+//     stock: 14});
 
-console.log( product.getProducts());
+// console.log( product.getProducts());
 
 // Busqueda por id
-console.log(product.getProductsById(2));
+// console.log(product.getProductsById(2));
 
 // busqueda de id inexistente
-console.log(product.getProductsById(6));
+// console.log(product.getProductsById(6));
 
 
 // Llamado a funcion para actualizar datos
-product.updateProduct(2, {
-    title: "Pedigree perro",
-    description: "alimento balanceado",
-    price: 26000,
-    thumbnail: "url-img",
-    code: 2001,
-    stock: 19
-});
+// product.updateProduct(2, {
+//     title: "Pedigree perro",
+//     description: "alimento balanceado",
+//     price: 26000,
+//     thumbnail: "url-img",
+//     code: 2001,
+//     stock: 19
+// });
 
 
-product.writeToFile(path);
+// product.writeToFile(path);
 
 
-const read = async ()=>{
-    try{
-        let resp = await fs.promises.readFile(path, 'utf-8')
-        console.log('Lectura de archivo txt: ', resp)
-    }catch(err){
-        console.log('Error: ', err)
-    }
-}
+// const read = async ()=>{
+//     try{
+//         let resp = await fs.promises.readFile(path, 'utf-8')
+//         console.log('Lectura de archivo txt: ', resp)
+//     }catch(err){
+//         console.log('Error: ', err)
+//     }
+// }
 
-read()
+// product.readFile(path);
+
+module.exports = ProductManager
--- index.js
+++ index.js
@@ -1,6 +1,36 @@
 const express = require('express')
+const ProductManager = require('./ProductManager')
 
 const app = express()
+let productos = new ProductManager
+
+app.get('/', ( req, res)=> {
+    res.send('Bienvenido a tales petshop');
+})
+
+app.get("/Productos", async (req, res)=> {
+    let resp = await productos.readFile();
+    // const parsedResp = JSON.parse(resp);
+    res.send(resp);
+})
+
+app.get("/producto/:id", async (req, res) => {
+    let id = parseInt(req.params.id)
+    let productoFnd = productos.find( (producto) => {
+        return producto.id === id
+    })
+    if (productoFnd) {
+        res.json(productoFnd);
+    } else {
+        res.status(404).json({ error: 'Producto no encontrado' });
+    }
+
+    // productos.readFileById(id)
+    // .then( (product) => {
+    //     if (product) { res.send(product)
+    //     } else {'Producto no encontrado'}
+    // })
+})
 
 app.listen(8080, ()=> {
     console.log('server run on port 8080')
